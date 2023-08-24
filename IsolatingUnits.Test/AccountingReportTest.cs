2ed43ad4e4aaadf86eaac4e6043d99a9742609c1
Sebastian Heimfarth <S.Heimfarth@smf.de>
2023-08-24 20:37:54 +0200

    Lösung für Aufgabe 1

--- IsolatingUnits.Test/AccountingReportTest.cs
+++ IsolatingUnits.Test/AccountingReportTest.cs
@@ -6,6 +6,41 @@ namespace IsolatingUnits.Test
 {
     public class AccountingReportTest
     {
-        
+        private static readonly DateTime TestDay = new (2021, 1, 1);
+        private DbContext? _dbContext;
+        private FtpImport? _ftpImport;
+        private FileStore? _fileStore;
+        private ILogger<AccountingReport>? _logger;
+        private AccountingReport? _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dbContext = new DbContext();
+            _ftpImport = new FtpImport();
+            _fileStore = new FileStore();
+            _logger = new DummyLogger<AccountingReport>();
+            
+            _sut = new AccountingReport(_dbContext, _ftpImport, _fileStore, _logger);
+        }
+
+        [Test]
+        public void CreateReport_CustomerAvailable_ShouldCreateReport()
+        {
+            // Act
+            var result = _sut!.CreateReport(Guid.Parse("9ed58d64-e654-472b-9001-c9da00f081cf"), TestDay);
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.Count().ShouldBeGreaterThan(0);
+        }
+
+        [Test]
+        public void CreateReport_CustomerNotAvailable_ShouldThrow()
+        {
+            // Act/Assert
+            Should.Throw<ArgumentException>(() => _sut!.CreateReport(Guid.Empty, TestDay));
+
+        }
     }
 }
---
+++ IsolatingUnits.Test/DummyLogger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace IsolatingUnits.Test;
+
+public class DummyLogger<T> : ILogger<T>
+{
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        //do nothing
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return false;
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return new Scope();
+    }
+
+    private class Scope : IDisposable
+    {
+        public void Dispose()
+        {
+            //do nothing
+        }
+    }
+}
